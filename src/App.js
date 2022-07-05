94c495798e511c356e4bd8b3a76a6ab1df16f03e
nasubibocchi <matsuhito125@gmail.com>
2022-07-05 21:33:26 +0900

    Revert "Connect web to metamask account"
    This reverts commit 26558a0e3b80ee5677d35c04e01ee8b8abde2c8c.

--- src/App.js
+++ src/App.js
@@ -1,96 +1,29 @@
-import React, { useEffect, useState } from "react";
+import * as React from "react";
 import { ethers } from "ethers";
-import "./App.css";
+import './App.css';
 
-const App = () => {
-  const [currentAccount, setCurrentAccount] = useState("");
-  console.log("currentAccount: ", currentAccount);
+export default function App() {
 
-  // window.ethereum にアクセスできることを確認
-  const checkIfWalletIsConnected = async () => {
-    try {
-      const { ethereum } = window;
-      if (!ethereum) {
-        console.log("Make sure you have MetaMask!");
-        return;
-      } else {
-        console.log("We have the ethereum object", ethereum);
-      }
+  const wave = () => {
 
-      // ユーザーのウォレットへのアクセス許可確認
-      const accounts = await ethereum.request({ method: "eth_accounts" });
-      if (accounts.length !== 0) {
-        const account = accounts[0];
-        console.log("Found an authorized account:", account);
-        setCurrentAccount(account);
-      } else {
-        console.log("No authorized account found");
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  }
 
-  // connectWalletメソッドを実装
-  const connectWallet = async () => {
-    try {
-      const { ethereum } = window;
-      if (!ethereum) {
-        alert("Get MetaMask!");
-        return;
-      }
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      console.log("Connected: ", accounts[0]);
-      setCurrentAccount(accounts[0]);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  // waveの回数をカウントする関数を実装
-  const wave = async () => {};
-
-  /* WEBページがロードされたときに下記の関数を実行します */
-  useEffect(() => {
-    checkIfWalletIsConnected();
-  }, []);
   return (
     <div className="mainContainer">
+
       <div className="dataContainer">
         <div className="header">
-          <span role="img" aria-label="hand-wave">
-            👋
-          </span>{" "}
-          WELCOME!
+        <span role="img" aria-label="hand-wave">👋</span> WELCOME!
         </div>
+
         <div className="bio">
-          イーサリアムウォレットを接続して、「
-          <span role="img" aria-label="hand-wave">
-            👋
-          </span>
-          (wave)」を送ってください
-          <span role="img" aria-label="shine">
-            ✨
-          </span>
+        イーサリアムウォレットを接続して、メッセージを作成したら、<span role="img" aria-label="hand-wave">👋</span>を送ってください<span role="img" aria-label="shine">✨</span>
         </div>
-        <button className="waveButton" onClick={null}>
-          Wave at Me
+
+        <button className="waveButton" onClick={wave}>
+        Wave at Me
         </button>
-        {/* ウォレットコネクトのボタンを実装 */}
-        {!currentAccount && (
-          <button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
-          </button>
-        )}
-        {currentAccount && (
-          <button className="waveButton" onClick={connectWallet}>
-            Wallet Connected
-          </button>
-        )}
       </div>
     </div>
   );
-};
-export default App;
+}
